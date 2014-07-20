9e6032b2e34d9aa6446d0a1f85bc938296bcd51e
bennythejudge <logube@gmail.com>
2014-07-20 09:53:40 +0100

    after adding the nav array of hashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,28 @@
 module ApplicationHelper
+  # third stage in the evolution of the view: introducing an array of hashes to 
+  # handle the nav items
+  def nav_items
+    [
+      {
+        displaytext: 'Companies',
+        controllername: 'companies',
+        linkurl: companies_path
+      },
+      {
+        displaytext: 'Projects',
+        controllername: 'projects',
+        linkurl: projects_path
+      },
+      {
+        displaytext: 'Works',
+        controllername: 'works',
+        linkurl: works_path
+      },
+    ]
+  end
+  
+  
+  
   def display_nav_item (displaytext, controllername, linkurl)
     # if the current controller name == controller name, then add the class active to the <li> tag plus the rest
     # otherwise don't add the class - add nothing
